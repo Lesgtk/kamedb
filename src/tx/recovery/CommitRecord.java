f21caedce3a6baf9b4495f7e8086919741b0929c
TakahiroKamei <lesgtk1510@gmail.com>
2022-09-04 23:12:10 +0900

    COMMITのLogRecord

---
+++ src/tx/recovery/CommitRecord.java
@@ -0,0 +1,39 @@
+package tx.recovery;
+
+import file.Page;
+import log.LogMgr;
+import tx.Transaction;
+
+public class CommitRecord implements LogRecord {
+	private int txnum;
+	
+	public CommitRecord(Page p) {
+		int tpos = Integer.BYTES;
+		txnum = p.getInt(tpos);
+	}
+	
+	public int op() {
+		return COMMIT;
+	}
+	
+	public int txNumber() {
+		return txnum;
+	}
+	
+	public void undo(Transaction tx) {
+		
+	}
+	
+	public String toString() {
+		return "<COMMIT" + txnum + ">";
+	}
+	
+	public static int writeToLog(LogMgr lm, int txnum) {
+		byte[] rec = new byte[2*Integer.BYTES];
+		Page p = new Page(rec);
+		p.setInt(0, COMMIT);
+		p.setInt(Integer.BYTES, txnum);
+		return lm.append(rec);
+	}
+
+}
